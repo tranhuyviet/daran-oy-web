771cc69dbbfda41f0091f0c05695fd633962fd8b
Viet Tran <tranhuyviet@gmail.com>
2019-10-21 18:04:11 +0300

    complete footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,63 @@
 import React from "react";
 
+import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
+
+import "./Footer.css";
+
 const Footer = () => {
-  return <div>hello from footer</div>;
+  return (
+    <footer className="section footer">
+      {/* footer links */}
+      <div className="footer-links">
+        <a href="#home" className="footer-link scroll-link">
+          home
+        </a>
+        <a href="#about" className="footer-link scroll-link">
+          about
+        </a>
+        <a href="#services" className="footer-link scroll-link">
+          services
+        </a>
+        <a href="#featured" className="footer-link scroll-link">
+          featured
+        </a>
+        <a href="#gallery" className="footer-link scroll-link">
+          gallery
+        </a>
+      </div>
+
+      {/* footer icons */}
+      <div className="footer-icons">
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          className="footer-icon"
+        >
+          <FaFacebook />
+        </a>
+        <a
+          href="https://www.twitter.com"
+          target="_blank"
+          className="footer-icon"
+        >
+          <FaTwitter />
+        </a>
+        <a
+          href="https://www.linkedin.com"
+          target="_blank"
+          className="footer-icon"
+        >
+          <FaLinkedin />
+        </a>
+      </div>
+
+      {/* footer copyright */}
+      <p className="copyright">
+        copyright &copy; Daran Oy {new Date().getFullYear()}. All rights
+        reserved.
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
